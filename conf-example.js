4cef66057c643294b9a582f37a871e4f11a37cdf
rainkolwa <jeremy199003@gmail.com>
2017-03-31 16:15:09 +0800

    Update conf-example.js

--- conf-example.js
+++ conf-example.js
@@ -1,41 +1,63 @@
 const _ = require('lodash')
 const moment = require('moment')
-const ncp = require('copy-paste')
 const os = require('os')
 
-let conf = {
-  repo: '/Users/xream/Project/Work/BlueOak/future-admin-vue',
-  number: 65535,
-  author: 'Hsiaoyi Hsu <xreamxu@gmail.com>',
-  fields: ['subject', 'body', 'committerDateRel', 'committerDate'],
-  all: true,
-  nameStatus: false,
-
-  week: true,
-  parser: commits => {
-    const report = _.chain(commits)
-      .reverse()
-      .map(({ body }) => {
-        return body.replace(/\[.*?\]|未来学校后台|\n/gi, '').split(/;|\.\s+?|；|。/)
-      })
-      .flatten()
-      .map(_.trim)
-      .filter(i=>i.length > 0 && !i.match(/merge|^(test|测试)$/img))
-      .map(i => '未来学校后台: ' + i)
-      .join(os.EOL)
-      .value()
-    console.log(report)
-    ncp.copy(report)
-  },
+const myConfig = {
+  data: [
+    {
+      repo: '/Users/Rain/Projects/future-web',
+      author: 'rainkolwa <pluvo.lee@gmail.com>',
+      project: '未来学校'
+    },
+    {
+      repo: '/Users/Rain/Projects/future-admin-vue',
+      author: 'rainkolwa <pluvo.lee@gmail.com>',
+      project: '未来学校后台'
+    }
+  ]
 }
-if (conf.week) {
-  const day = moment().day()
-  _.assign(conf, {
-    after: moment().subtract(day + 2, 'd').format('YYYY-MM-DD 15:00:00'),
-    before: moment().add(5 - day, 'd').format('YYYY-MM-DD 15:00:00'),
+
+const configure = (c) => {
+  let conf = {
+    repo: c.repo,
     number: 65535,
-  })
-}
-_.assign(conf, process.env)
+    author: c.author,
+    fields: ['subject', 'body', 'committerDateRel', 'committerDate'],
+    all: true,
+    nameStatus: false,
 
-module.exports = conf
+    week: true,
+    parser: commits => {
+      const report = _.chain(commits)
+        .reverse()
+        .map(({ body }) => {
+          let rule = "\\[.*?\\]|" + c.project + "|\\n"
+          let reg = new RegExp(rule, 'gi')
+          return body.replace(reg, '').split(/;|\.\s+?|；|。/)
+        })
+        .flatten()
+        .map(_.trim)
+        .filter(i=>i.length > 0 && !i.match(/merge|^(test|测试)$/img))
+        .map(i => c.project+ ': ' + i)
+        .join(os.EOL)
+        .value()
+      return report
+    },
+  }
+  if (conf.week) {
+    const day = moment().day()
+    _.assign(conf, {
+      after: moment().subtract(day + 2, 'd').format('YYYY-MM-DD 15:00:00'),
+      before: moment().add(5 - day, 'd').format('YYYY-MM-DD 15:00:00'),
+      number: 65535,
+    })
+  }
+  _.assign(conf, process.env)
+  return conf
+}
+
+const configs = myConfig.data.map(config => {
+  return configure(config)
+})
+
+module.exports = configs
